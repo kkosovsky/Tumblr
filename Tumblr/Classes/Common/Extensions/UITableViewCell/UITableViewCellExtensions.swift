b02112692af98ed57715a447a0efc6cc4994d570
Kosowski Kamil <kamil.kosowski@grupawp.pl>
2017-01-11 17:24:26 +0100

    Add UITableViewCell extensions

---
+++ Tumblr/Classes/Common/Extensions/UITableViewCell/UITableViewCellExtensions.swift
@@ -0,0 +1,11 @@
+//
+//  UITableViewCellExtensions.swift
+//  RandomUser
+//
+//  Created by Kamil on 29.12.2016.
+//  Copyright © 2016 Kamil. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell: ReusableView {}
