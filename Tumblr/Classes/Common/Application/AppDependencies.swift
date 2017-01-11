46fdb121300747cfa7a24ab88405dd36e0395894
Kosowski Kamil <kamil.kosowski@grupawp.pl>
2017-01-11 16:45:50 +0100

    Configure initial module essentials
    - Added AppDependencies class for initial dependency injection
    - Add RootWireframe
    - Add PostsListWireframe
    
    [ticket: 001]

---
+++ Tumblr/Classes/Common/Application/AppDependencies.swift
@@ -0,0 +1,31 @@
+//
+//  AppDependencies.swift
+//  Tumblr
+//
+//  Created by Kamil Kosowski on 11.01.2017.
+//  Copyright © 2017 Kamil Kosowski. All rights reserved.
+//
+
+import UIKit
+
+
+class AppDependencies {
+    
+    var postsListWireframe = PostsListWireframe()
+    
+    init(forAppDelegate appDelegate: AppDelegate) {
+        appDelegate.window = UIWindow(frame: UIScreen.main.bounds)
+        appDelegate.window?.makeKeyAndVisible()
+        configureDependencies()
+    }
+    
+    func installRootViewController(inWindow window: UIWindow?) {
+        guard let window = window else { return }
+        postsListWireframe.presentPostsListInterface(inWindow: window)
+    }
+    
+     private func configureDependencies() {
+    
+    }
+    
+}
