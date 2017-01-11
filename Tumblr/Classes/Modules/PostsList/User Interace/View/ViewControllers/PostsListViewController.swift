bc53f56cd8e555cf18102c2a8f6a257efc739aac
Kosowski Kamil <kamil.kosowski@grupawp.pl>
2017-01-11 16:51:10 +0100

    Add PostsListViewController

---
+++ Tumblr/Classes/Modules/PostsList/User Interace/View/ViewControllers/PostsListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PostsListViewController.swift
+//  Tumblr
+//
+//  Created by Kamil Kosowski on 11.01.2017.
+//  Copyright © 2017 Kamil Kosowski. All rights reserved.
+//
+
+import UIKit
+
+class PostsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
